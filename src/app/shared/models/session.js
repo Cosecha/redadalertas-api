d52c83ce701c03e392376e8b927efdc399f1e3ee
Celso Mireles <celso.mireles@gmail.com>
2017-02-14 03:45:46 -0700

    Adds authenticated key to session model

--- src/app/shared/models/session.js
+++ src/app/shared/models/session.js
@@ -1,9 +1,10 @@
 import mongoose, { Schema } from 'mongoose';
 
 export const sessionSchema = new Schema({
-  userID: { type: Schema.Types.ObjectId, ref: 'User' },
+  userID: String,
   accessToken: String,
   accessLevel: Number,
+  authenticated: Boolean,
 });
 
 export default mongoose.model('Session', sessionSchema);
