a6d67b68a66cbfed94c7cf9cd376e7886a3e1dfe
Celso Mireles <celso.mireles@gmail.com>
2017-02-14 19:15:30 -0700

    Sets up npm project in docs

---
+++ docs/index.js
@@ -0,0 +1,11 @@
+// Load bootprint
+require('bootprint')
+  // Load bootprint-swagger
+  .load(require('bootprint-openapi'))
+  // Customize configuration, override any options
+  .merge(require('./bootprint-config.js'))
+  // Specify build source and target
+  .build('main.yaml', 'gh-pages')
+  // Generate swagger-documentation into "target" directory
+  .generate()
+  .done(console.log)
