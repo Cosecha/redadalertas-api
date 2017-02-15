902dac3ce133d0443c5324d4cb25794666e43660
Celso Mireles <celso.mireles@gmail.com>
2017-02-14 19:18:03 -0700

    Remove everything

--- src/app/api/routes/index.js
+++
@@ -1,3 +0,0 @@
-import userRoutes from './user';
-
-export default [].concat(userRoutes);
--- src/app/shared/models/user.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-
-export const userSchema = new Schema({
-  phoneNumber: String,
-  receiveAlerts: Boolean,
-  hasReported: Boolean,
-});
-
-export default mongoose.model('User', userSchema);
