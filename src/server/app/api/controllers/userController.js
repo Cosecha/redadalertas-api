194535c846b17aa15b7849a8f1099d2f42d9b710
bianca c [arghgr] <tenmillionpeople@gmail.com>
2019-01-09 00:42:24 -0500

    userController: better error handling

--- src/server/app/api/controllers/userController.js
+++ src/server/app/api/controllers/userController.js
@@ -1,62 +1,74 @@
+import mongoose from 'mongoose';
 import Boom from 'boom';
+import Bounce from 'bounce';
+import { err as error } from '../../shared/utils';
 import userStore from '../stores/userStore';
+
+const ObjectId = mongoose.Types.ObjectId;
 
 const userController = {
   async createUser(req, h) {
     let user;
     try {
       user = await userStore.createUser(req.payload);
+      if (!user) throw new Error("User returned empty.");
       const response = h.response(user);
       return response;
     } catch (err) {
-      console.error("createUser error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("userController createUser error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error creating user.");
     }
   },
   async getUser(req, h) {
     let user;
     try {
-      user = await userStore.getUser(req.params.userID);
+      user = await userStore.getUser(ObjectId(req.params.userID));
+      if (!user) throw new Error("User not found.");
       const response = h.response(user);
       return response;
     } catch (err) {
-      console.error("getUser error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("userController getUser error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error retrieving user.");
     }
   },
   async getUsers(req, h) {
     let users;
     try {
       users = await userStore.getUsers();
+      if (!users) throw new Error("No users found.");
       const response = h.response(users);
       return response;
     } catch (err) {
-      console.error("getUsers error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("userController getUsers error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error retrieving users.");
     }
   },
   async updateUser(req, h) {
     let user;
     let updatedUser;
     try {
+      if (!req.payload || !req.payload._id) throw new Error("Did not receive valid information for update.");
+      if (!ObjectId.isValid(req.payload._id)) throw new Error("User ID is not valid.");
       user = await userStore.updateUser(req.payload);
+      if (!user) throw new Error("User not found.");
       updatedUser = await userStore.getUser(user.id);
       const response = h.response(updatedUser);
       return response;
     } catch (err) {
-      console.error("updateUser error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("userController updateUser error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error updating user.");
     }
   },
   async deleteUser(req, h) {
-    let res;
+    let user;
     try {
-      res = await userStore.deleteUser(req.params.userID);
-      const response = h.response(res);
+      user = await userStore.deleteUser(ObjectId(req.params.userID));
+      if (!user) throw new Error("User not found.");
+      const response = h.response(user);
       return response;
     } catch (err) {
-      console.error("deleteUser error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("userController deleteUser error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error deleting user.");
     }
   }
 };
