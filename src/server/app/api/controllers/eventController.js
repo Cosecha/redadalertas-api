47f333ad030818f38ba6d9c4b176c1ec7191f9be
bianca c [arghgr] <tenmillionpeople@gmail.com>
2019-01-08 20:19:49 -0500

    eventController: better error handling

--- src/server/app/api/controllers/eventController.js
+++ src/server/app/api/controllers/eventController.js
@@ -1,62 +1,74 @@
+import mongoose from 'mongoose';
 import Boom from 'boom';
+import Bounce from 'bounce';
+import { err as error } from '../../shared/utils';
 import eventStore from '../stores/eventStore';
+
+const ObjectId = mongoose.Types.ObjectId;
 
 const eventController = {
   async createEvent(req, h) {
     let event;
     try {
       event = await eventStore.createEvent(req.payload);
+      if (!event) throw new Error("Event returned empty.");
       const response = h.response(event);
       return response;
     } catch (err) {
-      console.error("createEvent error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("eventController createEvent error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error creating event.");
     }
   },
   async updateEvent(req, h) {
     let event;
     let updatedEvent;
     try {
+      if (!req.payload || !req.payload._id) throw new Error("Did not receive valid information for update.");
+      if (!ObjectId.isValid(req.payload._id)) throw new Error("Not a valid event ID.");
       event = await eventStore.updateEvent(req.payload);
+      if (!event) throw new Error("No event found.");
       updatedEvent = await eventStore.getEvent(event.id);
       const response = h.response(updatedEvent);
       return response;
     } catch (err) {
-      console.error("updateEvent error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("eventController updateEvent error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error updating event.");
     }
   },
   async getEvents(req, h) {
     let events;
     try {
       events = await eventStore.getEvents();
+      if (!events) throw new Error("No events found.");
       const response = h.response(events);
       return response;
     } catch (err) {
-      console.error("getEvents error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("eventController getEvents error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error retrieving events.");
     }
   },
   async getEvent(req, h) {
     let event;
     try {
-      event = await eventStore.getEvent(req.params.eventID);
+      event = await eventStore.getEvent(ObjectId(req.params.eventID));
+      if (!event) throw new Error("No event found.");
       const response = h.response(event);
       return response;
     } catch (err) {
-      console.error("getEvent error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("eventController getEvent error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error retrieving event.");
     }
   },
   async deleteEvent(req, h) {
-    let res;
+    let event;
     try {
-      res = await eventStore.deleteEvent(req.params.eventID);
-      const response = h.response(res);
+      event = await eventStore.deleteEvent(ObjectId(req.params.eventID));
+      if (!event) throw new Error("No event found.");
+      const response = h.response(event);
       return response;
     } catch (err) {
-      console.error("deleteEvent error: ", err);
-      return h.response(Boom.badRequest(err));
+      if (Bounce.isSystem(err)) error("eventController deleteEvent error: ", err.message || err);
+      return Boom.badRequest(err.message || "Error deleting event.");
     }
   }
 };
