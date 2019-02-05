241c1244e033e0065286b61cbe1b72fd13e89185
bianca c [arghgr] <tenmillionpeople@gmail.com>
2019-02-04 22:18:21 -0500

    Switch greenlock-hapi to greenlock-express
    the alias for greenlock-express isn't working

--- src/server/greenlock.js
+++ src/server/greenlock.js
@@ -1,24 +1,24 @@
-'use strict';
 import env from 'dotenv';
-import { log, logErr } from '../server/app/shared/utils';
+import { log, logErr } from './app/shared/utils';
+import * as greenlockExpress from 'greenlock-express';
 
 env.config();
 
 // API_DOMAINS is a comma-delimited string of domains
-log(`API_DOMAINS: ${process.env.API_DOMAINS.split(',')}`);
+if (process.env.API_DOMAINS) log(`API_DOMAINS: ${process.env.API_DOMAINS.split(',')}`);
 
-const greenlock = require('greenlock-hapi').create({
+const greenlock = greenlockExpress.create({
   version: 'draft-11' // Let's Encrypt v2
   // You MUST change this to 'https://acme-v02.api.letsencrypt.org/directory' in production
 , server: 'https://acme-staging-v02.api.letsencrypt.org/directory'
 
 , email: ***REMOVED***
 , agreeTos: true
-, approveDomains: process.env.API_DOMAINS.split(',')
+, approveDomains: (process.env.API_DOMAINS) ? process.env.API_DOMAINS.split(',') : []
 
 , configDir: require('os').homedir() + '/acme/etc'
 
-//, debug: true
+, debug: true
 });
 
 export default greenlock;
