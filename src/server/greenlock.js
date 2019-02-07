309ba07809aba31e7b8112d199da501f4938980c
bianca c [arghgr] <tenmillionpeople@gmail.com>
2019-02-07 02:57:57 -0500

    Don't create greenlock if no api domains

--- src/server/greenlock.js
+++ src/server/greenlock.js
@@ -3,23 +3,28 @@ import { log, logErr } from './app/shared/utils';
 import * as greenlockExpress from 'greenlock-express';
 
 env.config();
+let greenlock;
 
-// API_DOMAINS is a comma-delimited string of domains
-if (process.env.API_DOMAINS) log(`API_DOMAINS: ${process.env.API_DOMAINS.split(',')}`);
+// API_DOMAINS environment variable is a comma-delimited string of domains
+if (process.env.API_DOMAINS && process.env.API_DOMAINS.split(",").length > 0) {
+  log(`API_DOMAINS: ${process.env.API_DOMAINS.split(',')}`);
+  greenlock = greenlockExpress.create({
+    version: 'draft-11' // Let's Encrypt v2
+    // You MUST change this to 'https://acme-v02.api.letsencrypt.org/directory' in production
+  , server: 'https://acme-staging-v02.api.letsencrypt.org/directory'
 
-const greenlock = greenlockExpress.create({
-  version: 'draft-11' // Let's Encrypt v2
-  // You MUST change this to 'https://acme-v02.api.letsencrypt.org/directory' in production
-, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'
+  , email: ***REMOVED***
+  , agreeTos: true
+  , approveDomains: process.env.API_DOMAINS.split(',')
 
-, email: ***REMOVED***
-, agreeTos: true
-, approveDomains: (process.env.API_DOMAINS) ? process.env.API_DOMAINS.split(',') : []
+    // Uses root in Docker container (/root/app/letsencrypt or ~/app/letsencrypt)
+  , configDir: require('os').homedir() + '/app/letsencrypt'
 
-  // Uses root in Docker container (/root/app/letsencrypt or ~/app/letsencrypt)
-, configDir: require('os').homedir() + '/app/letsencrypt'
-
-, debug: true
-});
+  , debug: true
+  });
+} else {
+  log("API_DOMAINS: none");
+  greenlock = null;
+}
 
 export default greenlock;
